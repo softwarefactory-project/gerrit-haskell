7bbc34a8c9745253a214ac61e728710d6d190f96
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-15 19:38:27 +0000

    Initial summon
    Change-Id: Ic6add806c49bccaf6e15d4ef1c62522d56eb9384

---
+++ src/Gerrit.hs
@@ -0,0 +1,13 @@
+-- |
+-- Copyright: (c) 2020 software factory dev
+-- SPDX-License-Identifier: Apache-2.0
+-- Maintainer: Software Factory Dev <softwarefactory-dev@redhat.com>
+--
+-- A gerrit client
+module Gerrit
+  ( someFunc,
+  )
+where
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
