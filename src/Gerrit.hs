057235a6b091a2640d8dafad716dde0673a3e31f
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-15 20:58:58 +0000

    Add initial structure with a getVersion function
    Change-Id: Iaa104ea012a3395e04262aad673eccabf8287dd6

--- src/Gerrit.hs
+++ src/Gerrit.hs
@@ -1,3 +1,12 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 -- |
 -- Copyright: (c) 2020 software factory dev
 -- SPDX-License-Identifier: Apache-2.0
@@ -5,9 +14,41 @@
 --
 -- A gerrit client
 module Gerrit
-  ( someFunc,
+  ( withClient,
+    getVersion,
+    GerritStatus (..),
   )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BSL
+import Data.Maybe
+import Data.Text (Text)
+import GHC.Generics (Generic)
+import Network.HTTP.Req
+
+data GerritClient
+  = GerritClient
+      { host :: Text,
+        path :: Text
+      }
+
+data GerritStatus = GerritStatus Text
+  deriving stock (Show, Generic)
+  deriving anyclass (FromJSON)
+
+gerritGet ::
+  (MonadHttp m, FromJSON b) =>
+  (Url 'Https -> Url scheme) ->
+  GerritClient ->
+  m b
+gerritGet pathCb (GerritClient {..}) =
+  do
+    resp <- req GET (pathCb (https host /: path)) NoReqBody lbsResponse mempty
+    pure $ fromJust $ decode $ BSL.drop 5 $ responseBody resp
+
+getVersion :: MonadHttp m => GerritClient -> m GerritStatus
+getVersion = gerritGet (\x -> x /: "config" /: "server" /: "version")
+
+withClient :: Text -> Text -> (GerritClient -> Req ()) -> IO ()
+withClient host path f = runReq defaultHttpConfig $ f (GerritClient {..})
