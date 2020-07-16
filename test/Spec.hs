29f45d823c322121afda583cfc391915d2bdaffc
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-16 01:21:15 +0000

    Add initial tests
    Change-Id: I8dc7fc9611cb4df44dd5aeac09bba41c0e0aed52

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,29 @@
 module Main (main) where
 
+import Data.Aeson (decode)
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BSL
+import Data.Maybe (isJust)
+import Gerrit.Data
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = putStrLn ("Test suite is not implemented" :: String)
+main = do
+  dataFile <- BSL.readFile "./test/data/GerritChange.json"
+  defaultMain (tests dataFile)
+
+tests :: ByteString -> TestTree
+tests dataFile = testGroup "Tests" [unitTests dataFile]
+
+unitTests :: ByteString -> TestTree
+unitTests dataFile =
+  testGroup
+    "FromJSON"
+    [ testCase "Test GerritChange.json"
+        $ assertBool "GerritChange is decoded"
+        $ isJust
+        $ ( (decode dataFile) ::
+              Maybe GerritChange
+          )
+    ]
