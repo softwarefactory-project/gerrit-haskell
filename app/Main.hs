88908536ab7886446aa3dea7f16aa22ede1faa19
Tristan Cacqueray <tdecacqu@redhat.com>
2020-07-20 18:19:40 +0000

    Add auth argument and gerritPost function
    This change enables posting action to gerrit REST API.
    
    Change-Id: Ie6cd0081206cd939e3d49bcdacfb564d3cfc610c

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,6 @@ module Main (main) where
 import Gerrit (getVersion, withClient)
 
 main :: IO ()
-main = withClient "https://softwarefactory-project.io/r/" $ \client -> do
+main = withClient "https://softwarefactory-project.io/r/" Nothing $ \client -> do
   version <- getVersion client
   print version
