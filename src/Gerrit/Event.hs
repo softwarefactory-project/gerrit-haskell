15fa3fb99f5159b03ed6988274d672b8bfcede8f
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-28 18:40:30 +0000

    Add Gerrit.Event data types
    Change-Id: I7c926701b9e2b57b934917672bb1397851119ae0

---
+++ src/Gerrit/Event.hs
@@ -0,0 +1,175 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Gerrit stream event data type
+module Gerrit.Event
+  ( User (..),
+    Change (..),
+    PatchSet (..),
+    Ref (..),
+    ChangeEventType (..),
+    ChangeEvent (..),
+    RefEventType (..),
+    RefEvent (..),
+    CommentEventType (..),
+    CommentEvent (..),
+    Event (..),
+  )
+where
+
+import Control.Applicative ((<|>))
+import Control.Monad (mzero)
+import Data.Aeson (FromJSON (..), Options (fieldLabelModifier), Value (String), defaultOptions, genericParseJSON)
+import Data.Char (toLower)
+import Data.Text (Text)
+import qualified Data.Text as Text
+import GHC.Generics (Generic)
+
+aesonOption :: Text -> Options
+aesonOption prefix = defaultOptions {fieldLabelModifier = recordToJson}
+  where
+    recordToJson = updateCase . drop (Text.length prefix)
+    updateCase [] = []
+    updateCase (x : xs) = toLower x : xs
+
+data ChangeEventType
+  = PatchsetCreated
+  | ChangeMerged
+  deriving (Show)
+
+instance FromJSON ChangeEventType where
+  parseJSON (String v) = case v of
+    "patchset-created" -> pure PatchsetCreated
+    "change-merged" -> pure ChangeMerged
+    _ -> mzero
+  parseJSON _ = mzero
+
+data ProjectEventType
+  = ProjectCreated
+  deriving (Show)
+
+instance FromJSON ProjectEventType where
+  parseJSON (String v) = case v of
+    "project-created" -> pure ProjectCreated
+    _ -> mzero
+  parseJSON _ = mzero
+
+data RefEventType
+  = RefUpdated
+  deriving (Show)
+
+instance FromJSON RefEventType where
+  parseJSON (String v) = case v of
+    "ref-updated" -> pure RefUpdated
+    _ -> mzero
+  parseJSON _ = mzero
+
+data User = User
+  { userName :: Maybe Text,
+    userEmail :: Maybe Text,
+    userUsername :: Maybe Text
+  }
+  deriving (Show, Generic)
+
+instance FromJSON User where
+  parseJSON = genericParseJSON $ aesonOption "user"
+
+data PatchSet = PatchSet
+  { patchSetNumber :: Int,
+    patchSetUploader :: User
+  }
+  deriving (Show, Generic)
+
+instance FromJSON PatchSet where
+  parseJSON = genericParseJSON $ aesonOption "patchSet"
+
+data Change = Change
+  { changeProject :: Text,
+    changeBranch :: Text,
+    changeSubject :: Text,
+    changeUrl :: Text,
+    changeOwner :: User
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Change where
+  parseJSON = genericParseJSON $ aesonOption "change"
+
+data ChangeEvent = ChangeEvent
+  { changeEventProject :: Text,
+    changeEventType :: ChangeEventType,
+    changeEventUploader :: Maybe User,
+    changeEventChange :: Change,
+    changeEventPatchSet :: PatchSet
+  }
+  deriving (Show, Generic)
+
+instance FromJSON ChangeEvent where
+  parseJSON = genericParseJSON $ aesonOption "changeEvent"
+
+data CommentEventType
+  = CommentAdded
+  deriving (Show)
+
+instance FromJSON CommentEventType where
+  parseJSON (String v) = case v of
+    "comment-added" -> pure CommentAdded
+    _ -> mzero
+  parseJSON _ = mzero
+
+data CommentEvent = CommentEvent
+  { commentEventProject :: Text,
+    commentEventType :: CommentEventType,
+    commentEventAuthor :: User,
+    commentEventChange :: Change,
+    commentEventPatchSet :: PatchSet
+  }
+  deriving (Show, Generic)
+
+instance FromJSON CommentEvent where
+  parseJSON = genericParseJSON $ aesonOption "commentEvent"
+
+data ProjectEvent = ProjectEvent
+  { projectEventProjectName :: Text,
+    projectEventHeadName :: Text,
+    projectEventType :: ProjectEventType
+  }
+  deriving (Show, Generic)
+
+instance FromJSON ProjectEvent where
+  parseJSON = genericParseJSON $ aesonOption "projectEvent"
+
+data Ref = Ref
+  { refOldRev :: Text,
+    refNewRev :: Text,
+    refRefName :: Text,
+    refProject :: Text
+  }
+  deriving (Show, Generic)
+
+instance FromJSON Ref where
+  parseJSON = genericParseJSON $ aesonOption "ref"
+
+data RefEvent = RefEvent
+  { refEventSubmitter :: Maybe User,
+    refEventRefUpdate :: Ref,
+    refEventType :: RefEventType
+  }
+  deriving (Show, Generic)
+
+instance FromJSON RefEvent where
+  parseJSON = genericParseJSON $ aesonOption "refEvent"
+
+data Event
+  = EventChange ChangeEvent
+  | EventComment CommentEvent
+  | EventProject ProjectEvent
+  | EventRef RefEvent
+  deriving (Show)
+
+instance FromJSON Event where
+  parseJSON v =
+    EventChange <$> parseJSON v
+      <|> EventComment <$> parseJSON v
+      <|> EventProject <$> parseJSON v
+      <|> EventRef <$> parseJSON v
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,6 +6,7 @@ import qualified Data.ByteString.Lazy as BSL
 import Data.Maybe (fromJust, isJust)
 import Gerrit
 import Gerrit.Data as Gerrit
+import qualified Gerrit.Event as Event
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -16,7 +17,15 @@ main =
     withClient "http://example.com/r" Nothing $ \client ->
       defaultMain (tests (zip files dataFiles) client)
   where
-    files = ["GerritChange.json", "ReviewResult.json", "ReviewInput.json"]
+    files = [
+      "ChangeEvent.json",
+      "ChangeEventComment.json",
+      "RefEvent.json",
+      "ProjectEvent.json",
+      "GerritChange.json",
+      "ReviewResult.json",
+      "ReviewInput.json"
+     ]
 
 tests :: [(FilePath, ByteString)] -> GerritClient -> TestTree
 tests dataFiles client = testGroup "Tests" $ [unitTests] <> encodingTests dataFiles client
@@ -36,6 +45,18 @@ encodingTests dataFiles client =
       [ testCase "Test GerritChange.json"
           $ assertBool "GerritChange is decoded"
           $ isChange (decode changeRaw),
+        testCase "Test ChangeEvent.json"
+          $ assertBool "ChangeEvent is decoded"
+          $ isChangeEvent (decode $ getRaw "ChangeEvent.json"),
+        testCase "Test ChangeEventComment.json"
+          $ assertBool "ChangeEvent is decoded"
+          $ isCommentEvent (decode $ getRaw "ChangeEventComment.json"),
+        testCase "Test ProjectEvent.json"
+          $ assertBool "ProjectEvent is decoded"
+          $ isProjectEvent (decode $ getRaw "ProjectEvent.json"),
+        testCase "Test RefEvent.json"
+          $ assertBool "RefEvent is decoded"
+          $ isRefEvent (decode $ getRaw "RefEvent.json"),
         testCase "Test ReviewResult.json"
           $ assertBool "ReviewResult is decoded"
           $ isReviewResult (decode reviewResultRaw),
@@ -64,6 +85,17 @@ encodingTests dataFiles client =
     isReviewResult :: Maybe ReviewResult -> Bool
     isReviewResult (Just _) = True
     isReviewResult Nothing = False
+    isChangeEvent :: Maybe Event.Event -> Bool
+    isChangeEvent (Just (Event.EventChange _)) = True
+    isChangeEvent _ = False
+    isCommentEvent :: Maybe Event.Event -> Bool
+    isCommentEvent (Just (Event.EventComment _)) = True
+    isCommentEvent _ = False
+    isProjectEvent (Just (Event.EventProject _)) = True
+    isProjectEvent _ = False
+    isRefEvent :: Maybe Event.Event -> Bool
+    isRefEvent (Just (Event.EventRef _)) = True
+    isRefEvent _ = False
     isChange :: Maybe GerritChange -> Bool
     isChange (Just _) = True
     isChange Nothing = False
